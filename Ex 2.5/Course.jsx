9fa781e2b6413c4f15e8331bf5e323614e2c0b74
blindapple21 <88338952+blindapple21@users.noreply.github.com>
2025-02-04 07:05:32 +0530

    Create Course.jsx

---
+++ Ex 2.5/Course.jsx
@@ -0,0 +1,38 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ name }) => {
+  return <h2>{name}</h2>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p><strong>Total of {totalExercises} exercises</strong></p>;
+};
+
+export default Course;
