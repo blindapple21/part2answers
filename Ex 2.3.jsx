802b61484614a465e6c655429fde4e3adbfb9b5e
blindapple21 <88338952+blindapple21@users.noreply.github.com>
2025-02-04 06:52:02 +0530

    Create Ex 2.3.jsx

---
+++ Ex 2.3.jsx
@@ -0,0 +1,67 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => {
+    console.log('Accumulating:', sum, 'Adding:', part.exercises);
+    return sum + part.exercises;
+  }, 0);
+  return <p><strong>Total of {totalExercises} exercises</strong></p>;
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
